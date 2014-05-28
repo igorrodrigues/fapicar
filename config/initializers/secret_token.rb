70724a38b18f9f8c0246e1fdd3dcd1008e176db0
igorrodrigues <lost-sm@hotmail.com>
2014-05-28 20:22:30 -0300

    1ª Versão FAPICAR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fapicar::Application.config.secret_key_base = '7266ef45bd44f83b3c36cefa42840fdf26645ba20e612f50f51a68053fb051f3fb90412ec554921f48b215f2c8ada38da5cec58cb3f33bb40d58ebba867f830f'
